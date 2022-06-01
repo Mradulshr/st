3b9d51f10bcd27c19a028656b6a905e4b3bfa676
pyrafintech <pyrafintech@gmail.com>
2022-06-01 14:24:56 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,149 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  // border: 1px solid blue;
+`;
+
+const Logo = styled.h1`
+  // padding: 20px;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: teal; #${(prop) => prop.color};
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  margin-right:20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  // border: 1px solid red;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  // border: 1px solid green;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Pyra Jewels</Logo>
+        <Desc>
+          Objects designed for the adornment of the body are called jewelry.
+          While modern jewelry is made of gold, silver, or platinum, often with
+          precious or semiprecious stones, it evolved from shells, animal teeth,
+          and other items used as body decoration in prehistoric times.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Women Fashion</ListItem>
+          <ListItem>Assessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contect</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          Noida sector 3, 3 flore.
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          +9129989121
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          Contect@pyraSquare
+        </ContactItem>
+        <Payment src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8OMsuZlxub1Fn9VpTXU364lV5UbC5aTWmQbxy7W53F96zW-t_tToTuGRAw4Pz0LE0dg&usqp=CAU" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+      title: "OUR DEAL",
+      desc: "DONT'T COMPROMISE ON STYLE! GET 30% OFF FOR NEW ARRIVALS...",
+      bg: "#EFFFFD",
+    },
+    {
+      id: 2,
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-girlgirlwomenyoung-girlshefemalechilddaughter-1421526894560cswkw.png",
+      title: "NEW ARRIVEL",
+      desc: "DONT'T COMPROMISE ON STYLE! GET 30% OFF FOR NEW ARRIVALS...",
+      bg: "#EFFFFD",
+    },
+    {
+      id: 3,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+      title: "NEW COLLECTION",
+      desc: "DONT'T COMPROMISE ON STYLE! GET 30% OFF FOR NEW ARRIVALS...",
+      bg: "#EFFFFD",
+    },
+  ];
+  
+  export const categories = [
+    {
+      id: 1,
+      img: "https://c0.wallpaperflare.com/preview/111/723/664/jewellery-chokker-gold-gold-jewellery-thumbnail.jpg",
+      title: "Gold Bangle",
+    },
+    {
+      id: 2,
+      img: "https://i0.wp.com/www.indiaretailing.com/wp-content/uploads/2020/11/Jewellery-3.jpg?fit=681%2C400&ssl=1",
+      title: "Gold Bangle",
+    },
+    {
+      id: 3,
+      img: "https://content3.jdmagicbox.com/comp/delhi/94/011pk040494/catalogue/sona-jewellers-pitampura-delhi-jewellery-showrooms-4a4e9hx.png",
+      title: "Gold Bangle",
+    },
+  ];
+  
+  export const popularProducts = [
+    {
+      id: 1,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 2,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 3,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 4,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 5,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 6,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 7,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+    {
+      id: 8,
+      img: "http://pngbackground.com/public/uploads/preview/indian-girl-png-hd-saree-for-photoshop-photo-editing-11636269826imgq1qvdwt.png",
+    },
+  ];
+  
